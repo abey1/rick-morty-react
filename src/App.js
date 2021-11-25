c9adbcf033fcbf4ea674674e842b1510f795a2ea
abey3 <abeybruck@gmail.com>
2021-11-25 17:20:29 +0300

    project structure created

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+
+function App() {
+  return (
+  <h1 className="text-center ubuntu  my-4">
+    Rick & Morthy <span className="text-primary">WiKi</span> 
+    </h1>);
+}
+export default App;
