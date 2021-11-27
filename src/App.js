c15790904765fdf807f1da9972b17ea80ea28120
jegnaw2021 <jegnawjegnaw@gmail.com>
2021-11-27 21:44:20 +0300

    getting api and destructuring it started in app.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Cards from "./components/Cards/Cards";
+import Filters from "./components/Filters/Filters";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fetchedData, updateFetchedData] = useState([]);
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  console.log(fetchedData);
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
       <h1 className="text-center ubuntu  my-4">
@@ -9,8 +24,16 @@ function App() {
       </h1>
       <div className="container">
         <div className="row">
-          <div className="col-3"></div>
-          <div className="col-8"></div>
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards />
+              <Cards />
+              <Cards />
+            </div>
+          </div>
         </div>
       </div>
     </div>
